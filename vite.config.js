1319529ddfcbc0d379749ad14726e7a79c0dd068
JosephMTC <yoshiyosnichi@hotmail.com>
2025-10-26 22:30:38 -0500

    arreglo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  
   base: '/Eclipse/',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
